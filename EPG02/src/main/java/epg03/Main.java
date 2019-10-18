dafa789adef132b440e4ce7cb340ae3405cffc9a
Renildod7 <renildo.melo@ccc.ufcg.edu.br>
2019-10-17 21:24:20 -0300

    .

--- EPG02/src/main/java/epg03/Main.java
+++ EPG02/src/main/java/epg03/Main.java
@@ -14,6 +14,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
@@ -26,13 +27,12 @@ import org.jgrapht.alg.scoring.BetweennessCentrality;
 import org.jgrapht.alg.scoring.ClosenessCentrality;
 import org.jgrapht.alg.scoring.ClusteringCoefficient;
 import org.jgrapht.alg.scoring.HarmonicCentrality;
-import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
-import org.jgrapht.alg.shortestpath.GraphMeasurer;
 import org.jgrapht.graph.DefaultUndirectedWeightedGraph;
 import org.jgrapht.io.EdgeProvider;
 import org.jgrapht.io.GmlImporter;
 import org.jgrapht.io.ImportException;
 import org.jgrapht.io.VertexProvider;
+
 
 
 
@@ -44,57 +44,66 @@ public class Main {
 		grafo = new DefaultUndirectedWeightedGraph<DefaultVertex,RelationshipEdge>(RelationshipEdge.class);
 		importGraphGML(grafo, "./src/main/resources/antcolony1000.gml");
 		
-  	   	// Compute Metrics
-  	   	System.out.println("-BETWEENESS CENTRALITY- ");
-  	   	BetweennessCentrality <DefaultVertex, RelationshipEdge> bc = 
-  	   		new BetweennessCentrality <> (grafo,true);
-  	   	printOrderedVertexMeasures (bc.getScores(),0,true);
-
-  	   	System.out.println("\n-ALPHA CENTRALITY- ");
-  	   	AlphaCentrality <DefaultVertex, RelationshipEdge> ac = 
-  	   		new AlphaCentrality <> (grafo,0.001);
-  	   	printOrderedVertexMeasures (ac.getScores(),0,true);
-	    
-  	   	System.out.println("\n-CLOSENESS CENTRALITY- ");
-  	   	ClosenessCentrality <DefaultVertex, RelationshipEdge> cc = 
-	   		new ClosenessCentrality <> (grafo);
-  	   	printOrderedVertexMeasures (cc.getScores(),0,true);
-	    
-  	   	System.out.println("\n-HARMONIC CENTRALITY- ");
-  	   	HarmonicCentrality <DefaultVertex, RelationshipEdge> hc = 
-	   		new HarmonicCentrality <> (grafo);
-  	   	printOrderedVertexMeasures (hc.getScores(),0,true);
-	    
-  	   	
-  	   	
-  	   	
-  	   	
-  	   	
-  	   	
-  	   	
-  	   	
-  	   	ClusteringCoefficient <DefaultVertex,RelationshipEdge> cluster = 
-	    	new ClusteringCoefficient <> (grafo);
-  	   	System.out.println("\n\nCluster Coefficient: " + cluster.getGlobalClusteringCoefficient());
-	    
-  	   	System.out.println("\nASSORTATIVITY: " + assortativityCoefficient(grafo));
-	   
-  	   	if (GraphTests.hasMultipleEdges(grafo)==false) {
-  	  	   	double E = (grafo.edgeSet()).size();
-  	  	   	double V = (grafo.vertexSet()).size();
-  	  	   	double density = (2*E) / (V*(V-1));
-  	  	   	System.out.println("\nDENSITY: " + density);   		
-  	   	}   
-  	   	GraphMeasurer <DefaultVertex, RelationshipEdge> g = new GraphMeasurer <> (grafo);
-	   
-  	   	System.out.println("\nDIAMETER: " + g.getDiameter());	
-  	   	System.out.println("RADIUS: " + g.getRadius());
-	   
-  	   	System.out.println("CENTER: " + g.getGraphCenter());
-  	   	System.out.println("PERIPHERY: " + g.getGraphPeriphery());
-  	   	System.out.println("\n-Eccentricity-");
-  	   	printOrderedVertexMeasures(g.getVertexEccentricityMap(),0,false);
+		centrais(grafo);
+		
+		agrupamento(grafo);
+		
+		System.out.println(assortativityCoefficient(grafo));
+		
+		
+		
 	}
+	
+	public static void agrupamento(Graph<DefaultVertex,RelationshipEdge> grafo) {
+		ClusteringCoefficient <DefaultVertex,RelationshipEdge> cluster = new ClusteringCoefficient <> (grafo);
+		
+		System.out.println(cluster.getGlobalClusteringCoefficient());
+	}
+	
+	public static void centrais(Graph<DefaultVertex,RelationshipEdge> grafo) {
+		ClosenessCentrality<DefaultVertex, RelationshipEdge> cc = new ClosenessCentrality<DefaultVertex, RelationshipEdge>(grafo);
+		
+		double valor = Double.MIN_VALUE;
+		DefaultVertex vertice = null;
+		Map<DefaultVertex, Double> map = cc.getScores();
+		
+		ArrayList<Entry<DefaultVertex, Double>> list = new ArrayList<>();
+		list.addAll(map.entrySet());
+		Collections.sort(list, new Comparator<Entry<DefaultVertex, Double>>()
+		
+		{
+			public int compare(Entry<DefaultVertex, Double> entry1, Entry<DefaultVertex, Double> entry2) {
+				return entry1.getValue().compareTo(entry2.getValue()) * -1;
+			}
+			
+		} );
+		
+		
+		for(int i = 0; i < 5; i++) {
+			System.out.println(Integer.parseInt(list.get(i).getKey().getId()) +1 );
+		}
+		
+	}
+	
+    static <V,E> double assortativityCoefficient (Graph <V, E> graph) {
+        // from: https://github.com/Infeligo/jgrapht-metrics/blob/master/src/main/java/org/jgrapht/metrics/AssortativityCoefficientMetric.java
+    	double edgeCount = graph.edgeSet().size();
+        double n1 = 0, n2 = 0, dn = 0;
+
+        for (E e : graph.edgeSet()) {
+            int d1 = graph.degreeOf(graph.getEdgeSource(e));
+            int d2 = graph.degreeOf(graph.getEdgeTarget(e));
+
+            n1 += d1 * d2;
+            n2 += d1 + d2;
+            dn += d1 * d1 + d2 * d2;
+        }
+        n1 /= edgeCount;
+        n2 = (n2 / (2 * edgeCount)) * (n2 / (2 * edgeCount));
+        dn /= (2 * edgeCount);
+        
+        return (n1 - n2) / (dn - n2);
+    }
 
 	
 	
@@ -128,115 +137,5 @@ public class Main {
 		return readergml;
 	}
 	
-    static public <V,E> Set <V> getTreeCentroidPoints (Graph <V,E> graph) {
-		// graph must be a Tree
-		if (GraphTests.isTree(graph)==false) {
-			return new HashSet <> ();
-		}
-        Set<Entry<V, Double>> set = getCutPointWeights(graph).entrySet();
-        List<Entry<V, Double>> list = new ArrayList<Entry<V, Double>>(set);
-		list = list.stream().sorted((e1,e2) -> (e1.getValue()).compareTo(e2.getValue())).collect(Collectors.toList());
-		double size = list.get(0).getValue();
-		return (list.stream().filter(e -> e.getValue().doubleValue() == size).collect(Collectors.toSet())).stream().map(e -> e.getKey()).collect(Collectors.toSet());
-    }
-    
-	static public <V,E> Map <V,Double> getCutPointWeights (Graph <V,E> graph) {
-		// graph must be a Tree
-		Map <V,Double> weights = new HashMap <> (); 
-		if (GraphTests.isTree(graph)==false) {
-			return weights;
-		}
-		BiconnectivityInspector <V, E> insp =
-				new BiconnectivityInspector <> (graph);
-		DijkstraShortestPath <V, E> paths = 
-				new DijkstraShortestPath <> (graph);
-
-		Iterator <V> it1 = insp.getCutpoints().iterator();
-		while (it1.hasNext()) {
-			V v1 = it1.next();
-			Iterator <V> it2 = Graphs.neighborListOf(graph, v1).iterator();
-            int size = 0;
-			while (it2.hasNext()) {
-                V v2 = it2.next();
-    			List <E> s = new ArrayList <>();
-                Iterator <V> it3 = graph.vertexSet().iterator();
-                while (it3.hasNext()) {
-                	V v3 = it3.next();
-                    if (paths.getPath(v2, v3).getVertexList().contains(v1)==false && graph.degreeOf(v3)==1) {
-        	            s.addAll( paths.getPath(v2, v3).getEdgeList().stream().filter(e -> s.contains(e)==false ).collect(Collectors.toList())); 
-                    }
-                }
-                if(s.size() >= size) { 
-                	size = s.size()+1; 
-                }
-			}
-            weights.put(v1,new Double(size));			
-		}
-		return weights;
-	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	static <V> void printOrderedVertexMeasures (Map <V,Double> M, int count, boolean descending) {
-		// count representa a quantidade de elementos que devem ser exibidos 
-		// em ordem decrescente do score. Se count = 0, ent�o todos ser�o exibidos
-        Set<Entry<V, Double>> set = M.entrySet();
-        List<Entry<V, Double>> list = new ArrayList<Entry<V, Double>>(set);
-        if (descending) {
-        	Collections.sort( list, new Comparator<Map.Entry<V, Double>>()
-        		{
-        			public int compare( Map.Entry<V, Double> o1, Map.Entry<V, Double> o2 ) {
-        				return (o2.getValue()).compareTo( o1.getValue() );
-        			}
-        		} );
-        } else {
-        	Collections.sort( list, new Comparator<Map.Entry<V, Double>>()
-    		{
-    			public int compare( Map.Entry<V, Double> o1, Map.Entry<V, Double> o2 ) {
-    				return (o1.getValue()).compareTo( o2.getValue() );
-    			}
-    		} );
-        }
-        if (count == 0) {
-        	count = list.size();
-        }
-        for (int i = 0; i<count; i++) {
-        	Entry<V,Double> e = list.get(i);
-        	System.out.print(e.getKey()+": "+ String.format("%.2f",(e.getValue()))+ "; ");
-        }
-	}
-	
-    static <V,E> double assortativityCoefficient (Graph <V, E> graph) {
-        // from: https://github.com/Infeligo/jgrapht-metrics/blob/master/src/main/java/org/jgrapht/metrics/AssortativityCoefficientMetric.java
-    	double edgeCount = graph.edgeSet().size();
-        double n1 = 0, n2 = 0, dn = 0;
-
-        for (E e : graph.edgeSet()) {
-            int d1 = graph.degreeOf(graph.getEdgeSource(e));
-            int d2 = graph.degreeOf(graph.getEdgeTarget(e));
-
-            n1 += d1 * d2;
-            n2 += d1 + d2;
-            dn += d1 * d1 + d2 * d2;
-        }
-        n1 /= edgeCount;
-        n2 = (n2 / (2 * edgeCount)) * (n2 / (2 * edgeCount));
-        dn /= (2 * edgeCount);
-        
-        return (n1 - n2) / (dn - n2);
-    }
-
 }
---
+++ EPG02/src/main/resources/grafo1.gml
@@ -0,0 +1,427 @@
+Creator	"yFiles"
+Version	"2.16"
+graph
+[
+	hierarchic	1
+	label	""
+	directed	1
+	node
+	[
+		id	0
+		label	"A"
+		graphics
+		[
+			x	61.213203435596405
+			y	95.04361633096961
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"A"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	1
+		label	"B"
+		graphics
+		[
+			x	15.0
+			y	175.08723266193923
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"B"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	2
+		label	"C"
+		graphics
+		[
+			x	15.0
+			y	15.0
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"C"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	3
+		label	"D"
+		graphics
+		[
+			x	153.63961030678925
+			y	95.04361633096961
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"D"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	4
+		label	"E"
+		graphics
+		[
+			x	260.0
+			y	95.04361633096961
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"E"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	5
+		label	"F"
+		graphics
+		[
+			x	-50.325000000000045
+			y	-65.15
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"F"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	6
+		label	"G"
+		graphics
+		[
+			x	-58.325000000000045
+			y	229.85
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"G"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	7
+		label	"H"
+		graphics
+		[
+			x	132.67499999999995
+			y	188.85
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"H"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	8
+		label	"I"
+		graphics
+		[
+			x	132.67499999999995
+			y	15.0
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"I"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	node
+	[
+		id	9
+		label	"J"
+		graphics
+		[
+			x	-65.32500000000005
+			y	82.35
+			w	30.0
+			h	30.0
+			type	"rectangle"
+			raisedBorder	0
+			fill	"#FFCC00"
+			outline	"#000000"
+		]
+		LabelGraphics
+		[
+			text	"J"
+			fontSize	12
+			fontName	"Dialog"
+			model	"null"
+		]
+	]
+	edge
+	[
+		source	0
+		target	1
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	0
+		target	2
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	0
+		target	3
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	4
+		target	3
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	6
+		target	1
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	5
+		target	2
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	3
+		target	7
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	1
+		target	7
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	0
+		target	7
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	9
+		target	1
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	9
+		target	2
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	9
+		target	0
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	2
+		target	8
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	8
+		target	3
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	8
+		target	0
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	4
+		target	7
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	4
+		target	8
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	6
+		target	7
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	6
+		target	9
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	9
+		target	5
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+	edge
+	[
+		source	5
+		target	8
+		graphics
+		[
+			fill	"#000000"
+		]
+	]
+]
